8a4668413ff8f3bda1b43fc96b9365d31a43819c
richardzarate <richardzarate01@gmail.com>
2024-12-09 01:35:41 -0800

    - updated Friend action function to return information about their action
    - updated gameMaster:
       -created createRandomCard function to randomly create card
       -created pullCardFromDeck to simulate the pulling of a random card from deck but in reality it's just putting a randomly created card in your hand
       - created RandomEnemyUnit function to create a random Enemy unit
       -created checkWinner function to check if there's a winner. Currently only tested a case when the enemy won
       - updated initializeFloor function to run with the new functions created
       - created default friend units
       - created static variables to help with random creation of card and enemy units
     - updated gamePlay scene to include as much information as possible for the player
     - updated gamePlay Controller to run the game
        - updated initialize function to populate the scene
        - created the onEnterClick function to run the game based on the information gathered from playerInputTextField
        - created the updateTextAreas to update information whenever a turn ends
        - created the makeMove function that will handle friend and unit interactions
        - checkCardsSelection and checkTargetsSelection was created to check if index is outofbounds
        - created the loadCards function to update cardInformation each turn
    
      TODO: need to debug playerInput some more but overall the game is playable
      TODO: check if index is tracked properly
      TODO: Add 1, 2, and 3 on the Friend side of the GamePlay scene

--- src/main/java/group5/startscreenud2balatro/Friend.java
+++ src/main/java/group5/startscreenud2balatro/Friend.java
@@ -7,32 +7,55 @@ public class Friend extends Unit{
     }
 
     //action function for Friend class depends on the unit and card stats plus if the card type matches the unit type
-    public void action(Card card, Unit target){
+    public String action(Card card, Unit target){
 
+        int actionValue = 0;
         //check if the card type and unit type matches first
         if(card.getActionType().equals(this.type)){
             if(card.getActionType().equals("heal")){
-                target.currentHealth += 2 * card.getActionValue();
+                actionValue = 2 * card.getActionValue();
+                target.currentHealth += actionValue;
+
+                return this.getName() + " has healed " + target.getName() + " for " + actionValue + " health points.";
             }
             else if(card.getActionType().equals("attack")){
-                target.currentHealth -= card.getActionValue() + (this.attack / target.defense);
+                if(target.getCurrentHealth() <= 0){
+                    return this.getName() + " has attacked an already dead target.";
+                }
+                actionValue = card.getActionValue() + (this.attack / target.defense);
+                target.currentHealth -= actionValue;
+
+                return this.getName() + " has attacked " + target.getName() + " for " + actionValue + " damage.";
             }
             else if(card.getActionType().equals("defend")){
-                this.defense += (2 * card.getActionValue()); //double the defense plus the card value if both card and unit type are defensive
+                actionValue = (2 * card.getActionValue()); //double the defense plus the card value if both card and unit type are defensive
+                this.defense += actionValue;
+                return this.getName() + " has raised defense by " + actionValue + " points.";
             }
 
         }
         //if they don't match do this instead
         else{
             if(card.getActionType().equals("heal")){
-                target.currentHealth += card.getActionValue();
+                actionValue = card.getActionValue();
+                target.currentHealth += actionValue;
+                return this.getName() + " has healed " + target.getName() + " for " + actionValue + " health points.";
             }
             else if(card.getActionType().equals("attack")){
-                target.currentHealth -= (card.getActionValue() / target.defense);
+                if(target.getCurrentHealth() <= 0){
+                    return this.getName() + " has attacked an already dead target.";
+                }
+                actionValue = (card.getActionValue() / target.defense);
+                target.currentHealth -= actionValue;
+                return this.getName() + " has attacked " + target.getName() + " for " + actionValue + " damage.";
+
             }
             else if(card.getActionType().equals("defend")){
-                this.defense += card.getActionValue();
+                actionValue = card.getActionValue();
+                this.defense += actionValue;
+                return this.getName() + " has raised defense by " + actionValue + " points.";
             }
         }
+        return "Invalid move...";
     }
 }
