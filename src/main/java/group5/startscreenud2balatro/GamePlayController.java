0531e788ab83cad25f98cfa8ecf324f1b4e3ee20
Gabriel Cavalle <gabecavalle@gmail.com>
2024-12-09 23:56:17 -0800

    Fixed bug that didn't print error message for number 6 being selected

--- src/main/java/group5/startscreenud2balatro/GamePlayController.java
+++ src/main/java/group5/startscreenud2balatro/GamePlayController.java
@@ -172,7 +172,14 @@ public class GamePlayController {
 
 
         try{
-            selectedIndex = Integer.parseInt(input) - 1;
+            selectedIndex = Integer.parseInt(input);
+
+            // validates card selection isn't larger than hand size
+            if (selectedIndex < 0 || selectedIndex >= game.getHand().size()) {
+                errorMessageLabel.setText("Invalid card selection. Please select a valid card.");
+                return;
+            }
+
             if(selectedCard == null){
 
                 selectedCard = game.getHand().get(selectedIndex);
