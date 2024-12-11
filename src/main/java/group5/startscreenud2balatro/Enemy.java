a319da53b6464a94ca6df85cc30f035acb04aa58
richardzarate <richardzarate01@gmail.com>
2024-12-10 18:35:34 -0800

    Reverted back original calculations for enemy actions

--- src/main/java/group5/startscreenud2balatro/Enemy.java
+++ src/main/java/group5/startscreenud2balatro/Enemy.java
@@ -2,7 +2,7 @@ package group5.startscreenud2balatro;
 
 public class Enemy extends Unit {
     public Enemy(String name, int health, int attack, int defense, String type){
-        super(name, health/5, attack/5, defense/5, type);
+        super(name, health, attack, defense, type);
     }
 
     //enemy unit action that depends on their health. Plus returns a string to state what they're doing
@@ -16,18 +16,18 @@ public class Enemy extends Unit {
             }
             else{
                 this.defense = (int)(this.defense * 1.1); //defensive stance
-                return this.name + " goes into defensive stance.\n";
+                return this.name + " goes into defensive stance.";
             }
 
         }
         //if enemy's health is high enough, it will attack instead
         else{
             if(target.getCurrentHealth() <= 0){
-                return this.getName() + " has attacked an already dead target.\n";
+                return this.getName() + " has attacked an already dead target.";
             }
             damage = (this.attack / target.defense);
             target.currentHealth -= damage;
-            return this.name + " attacks " + target.name + " for " + damage + " damage.\n";
+            return this.name + " attacks " + target.name + " for " + damage + " damage.";
         }
 
 
